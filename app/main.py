980e798ab9098ab62ccd5bff2efdc55d12411c70
kgh2001 <103418211+kgh2001@users.noreply.github.com>
2025-05-23 11:14:26 +0900

    Update main.py
    사용자 피드백을 받아 /data/raw에 저장하게 하는 부분 추가 했고, python -m app.main으로 실행되게 코드 수정했습니다

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 # app/main.py
 from flask import Flask, render_template, request, jsonify
-from utils.file_io import *
-from utils.preprocessing import *
-from model.predict import *
-from model.data_loader import *
+from app.utils.file_io import *
+from app.utils.preprocessing import *
+from app.model.predict import *
+from app.model.data_loader import *
+from app.utils.feedback import *
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -17,8 +20,18 @@ def predict():
         user_input = request.form['message']
         processed_input = preprocess_user_input(user_input)
         model_choice = request.form['model_choice']
-        prediction = predict_with_selected_model(processed_input, model_choice)
-        return render_template('index.html', input_text=user_input, prediction_text=prediction.replace('\n', '<br>'))
+        prediction, label = predict_with_selected_model(processed_input, model_choice, return_label=True)  # <- label도 리턴하도록
+        return render_template('index.html',
+                               input_text=user_input,
+                               prediction_text=prediction.replace('\n', '<br>'),
+                               prediction_label=label,
+                               model_choice=model_choice)
+
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    data = request.get_json()
+    result = process_feedback(data)
+    return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5050)
